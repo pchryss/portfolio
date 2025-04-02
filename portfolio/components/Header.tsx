a63e97e69c71002be7e57ca6d61de383571ea502
pchryss <philipchryssochoos@gmail.com>
2025-04-02 16:08:28 -0400

    Add social icons to header

--- portfolio/components/Header.tsx
+++ portfolio/components/Header.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import NasaAPOD from './NasaAPOD';
+import { Github, Linkedin, Mail } from 'lucide-react';
 
 const Header = () => {
   return (
     <div id="info" className="flex p-4 gap-5 flex-wrap items-center">
       <div>
-        <div className="flex flex-col gap-2 max-w-xl text-white">
+        <div className="flex flex-col gap-5 max-w-xl text-white">
           <h1 className="text-4xl">Hello, I'm Philip!</h1>
           <h2>
             I'm currently studying computer science at the Georgia Institute of
@@ -13,6 +14,20 @@ const Header = () => {
             Yellow Jacket Space Program and an active member of the Georgia Tech
             Astronomy Club!
           </h2>
+          <div className="flex gap-3">
+            <a href="https://github.com/pchryss" target="_blank">
+              <Github color="#ffffff" size={50} />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/philipchryssochoos/"
+              target="_blank"
+            >
+              <Linkedin color="#ffffff" size={50} />
+            </a>
+            <a href="mailto:philipchryssochoos.work@gmail.com" target="_blank">
+              <Mail color="#ffffff" size={50} />
+            </a>
+          </div>
         </div>
       </div>
       <NasaAPOD />
